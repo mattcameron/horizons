3a07bbd75216225851e482aac2a0e8629d41318b
Matt Cameron <mcameron@avalanche.com.au>
2015-05-06 09:56:33 +1000

    minor updates to make login and register work

--- main.rb
+++ main.rb
@@ -17,44 +17,41 @@ after do
 	ActiveRecord::Base.connection.close
 end
 
-  get '/' do
-    erb :index
-  end
+get '/' do
+  erb :index
+end
 
-  get '/register' do
-  	erb :register
-  end
+get '/register' do
+	erb :register
+end
 
-  get '/login' do
-  	erb :login
-  end
+get '/login' do
+	erb :login
+end
 
-  get '/race' do
-  	erb :race
-  end
+get '/race' do
+	erb :race
+end
 
-  get '/status' do
-  	erb :status
-  end
+get '/status' do
+	erb :status
+end
 
-  get '/api/checkpoints' do
-  	content_type :json
-  	checkpoints = Checkpoint.all
-  	checkpoints.to_json
-  end
+get '/api/checkpoints' do
+	content_type :json
+	checkpoints = Checkpoint.all
+	checkpoints.to_json
+end
 
-
-
-
-	post '/signup' do
-	@user = User.create( username: params[:username], email: params[:email], password: params[:password])
+# LOGIN
+post '/signup' do
+	@user = User.create( user_name: params[:username], email: params[:email], password: params[:password])
 		session[:user_id] = @user.id
 	redirect to ('/')
 end
 
 
 # SESSION STUFF
-
 post '/session' do
 	@user = User.where(email: params[:email]).first
 	if @user && @user.authenticate(params[:password])
@@ -71,6 +68,9 @@ delete '/session' do
 end
 
 
+
+
+# HELPERS
 
 helpers do
 	def logged_in?
