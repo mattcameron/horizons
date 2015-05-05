c742a73937c85ffc0eeea60a5ec2d03e376cb3fc
Matt Cameron <mcameron@avalanche.com.au>
2015-05-05 16:53:57 +1000

    Added tables needed

--- horizons.sql
+++ horizons.sql
@@ -1,22 +1,29 @@
 CREATE DATABASE horizons;
 
 CREATE TABLE checkpoints (
-	id SERIAL4 PRIMARY KEY,
-  name VARCHAR(50),
-  description VARCHAR(500),
-	latitude INTEGER NOT NULL,
-	longitude INTEGER NOT NULL,
+  id SERIAL4 PRIMARY KEY,
+	name VARCHAR(50),
+	description VARCHAR(500),
+  latitude NUMERIC NOT NULL,
+  longitude NUMERIC NOT NULL,
+  image_url VARCHAR(500)
 );
-
 
 CREATE TABLE users (
-	id SERIAL4 PRIMARY KEY,
-	user_name VARCHAR(30) NOT NULL,
-	email VARCHAR(200) NOT NULL,
-	password_digest VARCHAR(300) NOT NULL,
+  id SERIAL4 PRIMARY KEY,
+  user_name VARCHAR(30) NOT NULL,
+  email VARCHAR(200) NOT NULL,
+  password_digest VARCHAR(300) NOT NULL
 );
 
-CREATE TABLE checkpoint_users (
-	user_id INTEGER
-	location_id INTEGER
+CREATE TABLE races (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  created_at TIMESTAMP
+);
+
+CREATE TABLE checkpoints_races_users (
+  user_id INTEGER,
+  checkpoint_id INTEGER,
+  race_id INTEGER
 );
