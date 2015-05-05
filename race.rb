d83e1467e5b2b461c4dc23c46aacc29ef98876db
Matt Cameron <mcameron@avalanche.com.au>
2015-05-05 22:26:54 +1000

    updates to classes for ActiveRecord

--- race.rb
+++ race.rb
@@ -1,3 +1,5 @@
 class Race < ActiveRecord::Base
-
+	has_many :checkpoint_race_users
+	has_many :users, :through => :checkpoint_race_users
+	has_many :checkpoints, :through => :checkpoint_race_users
 end
--- user.rb
+++ user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	has_many :checkpoint_race_users
+	has_many :races, :through => :checkpoint_race_users
+	has_many :checkpoints, :through => :checkpoint_race_users
 end
