52a9416f278097d19b1d6e121f3742b6b2d92608
Dominic White <dominicpaulwhite@gmail.com>
2015-05-07 09:26:13 +1000

    function to adjust user checkpoint tally and progress bar

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,4 +12,51 @@ $('#join_race_btn').on('click', function() {
 	});
 })
 
+var race_id = 1;
+var user_id = 1;
+var currentRaceCheckpoints;
+var userCheckpoints;
+var elapsedTime;
+
+var setMeter = function() {
+	setTimeout(function(){
+		$('.meter').css('width', (userCheckpoints/currentRaceCheckpoints*100)+"%")},
+		500
+	);
+	setTimeout(function(){
+		$('.tally').html(userCheckpoints + "/" + currentRaceCheckpoints)},
+		500
+	);
+}
+
+var getRaceCheckpoints = function() {
+		$.ajax({
+	    url: "/api/get_race_cp/"+race_id,
+	    method: "get"
+	  }).done(function(data) {
+	  	currentRaceCheckpoints = data.length
+	  });
+}
+
+var getUserCheckpoints = function() {
+	 $.ajax({
+	    url: "/api/get_user_cp/"+user_id,
+	    method: "get"
+	  }).done(function(data) {
+	  	userCheckpoints = data.length
+	  });
+}
+
+var checkProgress = function() {
+	getUserCheckpoints();
+	getRaceCheckpoints();
+	setMeter();
+}
+
+
+$(document).ready(function(){
+	setInterval(checkProgress(), 10000);
+});
+
+
 
