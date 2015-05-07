1c2268b86625074fd223fafa020b9d0d9b8418d2
Beta Ngadiman <ThirtySevenCelsiusAir@Betas-MacBook-Air.local>
2015-05-07 14:25:36 +1000

    getting close to the horizons

--- public/javascripts/checkpoints.js
+++ public/javascripts/checkpoints.js
@@ -32,7 +32,8 @@ function initialize() {
   // Create the map defaults.
   var mapOptions = {
     zoom: 10,
-    center: new google.maps.LatLng(-37.818291, 144.962457)
+    center: new google.maps.LatLng(-37.818291, 144.962457),
+    styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
   };
 
   // declare the map as the 'map-canvas' div
@@ -51,10 +52,10 @@ function initialize() {
         strokeColor: '#000000',
         strokeOpacity: 0.8,
         strokeWeight: 0,
-        fillColor: '#0000FF',
-        fillOpacity: 0.7,
+        fillColor: '#2ea5b9',
+        fillOpacity: 0.6,
         map: map,
-        paths: drawCircle(new google.maps.LatLng(value.latitude, value.longitude),.03,1),
+        paths: drawCircle(new google.maps.LatLng(value.latitude, value.longitude),.02,1),
         id: value.id
       })
 
